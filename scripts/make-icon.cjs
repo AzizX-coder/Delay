f547beb689aeb0a735892efe2b4b91b54bccd61b
AzizX-coder <195313522+AzizX-coder@users.noreply.github.com>
2026-04-11 22:40:33 +0500

    Fix png-to-ico ES Module import for GitHub Actions

--- scripts/make-icon.cjs
+++ scripts/make-icon.cjs
@@ -1,11 +1,14 @@
-const pngToIco = require("png-to-ico").default;
 const fs = require("fs");
 const path = require("path");
 
 const pngPath = path.join(__dirname, "..", "public", "icon.png");
 const icoPath = path.join(__dirname, "..", "public", "icon.ico");
 
-pngToIco(pngPath)
+import("png-to-ico")
+  .then((module) => {
+    const pngToIco = module.default || module;
+    return pngToIco(pngPath);
+  })
   .then((buf) => {
     fs.writeFileSync(icoPath, buf);
     console.log("Created", icoPath);
